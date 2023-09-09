0b174a40ef37e6f977c9234dc89984d3c258705e
Andrey Golyachenko <leswar@yandex.ru>
2023-09-10 00:12:25 +0500

    Amendments.

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,21 @@
 int main()
 {
 	std::string command;
+	std::string site = "http://httpbin.org";
 	while (true)
 	{
 		std::cout << "Commands: \nGet\nPost\nPut\nDelete\nPatch\nExit\n Enter the command:  " << std::endl;
 		std::cin >> command;
 		if (command == "Get" || command == "get")
-			std::cout << cpr::Get(cpr::Url("http://httpbin.org/get")).text << std::endl;
+			std::cout << cpr::Get(cpr::Url(site + "/get")).text << std::endl;
 		else if (command == "Post" || command == "post")
-			std::cout << cpr::Post(cpr::Url("http://httpbin.org/post")).text << std::endl;
+			std::cout << cpr::Post(cpr::Url(site + "/post")).text << std::endl;
 		else if (command == "Put" || command == "put")
-			std::cout << cpr::Put(cpr::Url("http://httpbin.org/put")).text << std::endl;
+			std::cout << cpr::Put(cpr::Url(site + "/put")).text << std::endl;
 		else if (command == "Delete" || command == "delete")
-			std::cout << cpr::Delete(cpr::Url("http://httpbin.org/delete")).text << std::endl;
+			std::cout << cpr::Delete(cpr::Url(site + "/delete")).text << std::endl;
 		else if (command == "Patch" || command == "patch")
-			std::cout << cpr::Patch(cpr::Url("http://httpbin.org/patch")).text << std::endl;
+			std::cout << cpr::Patch(cpr::Url(site + "/patch")).text << std::endl;
 		else if (command == "Exit" || command == "exit") return 0;
 		else
 			std::cerr << "Error! Incorrect input command. Try again. " << std::endl;
